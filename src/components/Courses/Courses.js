0694a697bc6c798bdcaffc167a00b482e4278403
JubayerDev <ajashes858@gmail.com>
2022-08-25 19:23:49 +0600

    responsive design

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -11,10 +11,10 @@ const Courses = () => {
     },[])
     return (
         <section className='my-20'>
-            <div className='flex justify-around items-center'>
-                <div>
-                    <p className='text-[#F9835C] font-semibold'>Popular Courses</p>
-                    <h1 className="text-3xl font-bold tracking-wider">Trusted By Million</h1>
+            <div className='flex justify-around items-center lg:flex-row sm:flex-col'>
+                <div className=''>
+                    <p className='text-[#F9835C] lg:font-semibold lg:text-lg sm:text-sm'>Popular Courses</p>
+                    <h1 className="lg:text-3xl sm:text-xl font-bold tracking-wider">Trusted By Million</h1>
                 </div>
                 <div>
                     <button className='btn bg-[#32D78B] border-0 hover:bg-[#32D78B]'>View All Course</button>
@@ -22,7 +22,7 @@ const Courses = () => {
             </div>
 
             {/* Cards */}
-            <div className='grid grid-cols-3 gap-7 mx-[10%] mt-20'>
+            <div className='grid lg:grid-cols-3 sm:grid-cols-1 gap-7 mx-[10%] mt-20'>
                 {
                     courses.map(course => <Course course={ course} key={course.id} />)
                 }
--- src/components/Rules/Rules.js
+++ src/components/Rules/Rules.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const Rules = () => {
     return (
-        <div className='my-28 flex items-center w-[90vw] mx-auto'>
-            <div className='relative bottom-[-3rem]'>
+        <div className='my-28 grid lg:grid-cols-4 sm:grid-cols-1 w-[90vw] mx-auto'>
+            <div className='relative bottom-[-3rem] mb-28'>
                 <button className='btn bg-[#F38E71] border-0 hover:bg-[#F38E71] text-2xl absolute left-[43%] bottom-[95%]'>01</button>
                 <h2 className='text-xl font-semibold my-6 text-center'>Pick a Course</h2>
                 <p className='text-center'>Start a Show Academy Course of Your Choice For Free</p>
             </div>
-            <div className='relative'>
+            <div className='relative mb-5'>
                 <button className='btn bg-[#F0B867] border-0 hover:bg-[#F0B867] text-2xl absolute left-[43%] bottom-[95%]'>02</button>
                 <h2 className='text-xl font-semibold my-6 text-center'>Pick a Course</h2>
                 <p className='text-center'>Start a Show Academy Course of Your Choice For Free</p>
             </div>
-            <div className='relative bottom-[-3rem]'>
+            <div className='relative bottom-[-3rem] mb-28'>
                 <button className='btn bg-[#D379ED] border-0 hover:bg-[#D379ED] text-2xl absolute left-[43%] bottom-[95%]'>03</button>
                 <h2 className='text-xl font-semibold my-6 text-center'>Pick a Course</h2>
                 <p className='text-center'>Start a Show Academy Course of Your Choice For Free</p>
