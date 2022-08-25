09db673c97d75c91ca23ab9211250b25d4ba6330
JubayerDev <ajashes858@gmail.com>
2022-08-25 13:59:53 +0600

    rules added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import Courses from './components/Courses/Courses';
+import Rules from './components/Rules/Rules';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Header />
       <Courses />
+      <Rules />
     </div>
   );
 }
---
+++ src/components/Rules/Rules.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Rules = () => {
+    return (
+        <div className='my-28 flex items-center w-[90vw] mx-auto'>
+            <div className='relative bottom-[-3rem]'>
+                <button className='btn bg-[#F38E71] border-0 hover:bg-[#F38E71] text-2xl absolute left-[43%] bottom-[95%]'>01</button>
+                <h2 className='text-xl font-semibold my-6 text-center'>Pick a Course</h2>
+                <p className='text-center'>Start a Show Academy Course of Your Choice For Free</p>
+            </div>
+            <div className='relative'>
+                <button className='btn bg-[#F0B867] border-0 hover:bg-[#F0B867] text-2xl absolute left-[43%] bottom-[95%]'>02</button>
+                <h2 className='text-xl font-semibold my-6 text-center'>Pick a Course</h2>
+                <p className='text-center'>Start a Show Academy Course of Your Choice For Free</p>
+            </div>
+            <div className='relative bottom-[-3rem]'>
+                <button className='btn bg-[#D379ED] border-0 hover:bg-[#D379ED] text-2xl absolute left-[43%] bottom-[95%]'>03</button>
+                <h2 className='text-xl font-semibold my-6 text-center'>Pick a Course</h2>
+                <p className='text-center'>Start a Show Academy Course of Your Choice For Free</p>
+            </div>
+            <div className='relative'>
+                <button className='btn bg-[#3DE196] border-0 hover:bg-[#3DE196] text-2xl absolute left-[43%] bottom-[95%]'>04</button>
+                <h2 className='text-xl font-semibold my-6 text-center'>Pick a Course</h2>
+                <p className='text-center'>Start a Show Academy Course of Your Choice For Free</p>
+            </div>
+        </div>
+    );
+};
+
+export default Rules;
