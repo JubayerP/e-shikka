7c9cd6dc727b6d72b63775700e36788d85915f5f
JubayerDev <ajashes858@gmail.com>
2022-08-25 21:16:34 +0600

    responsiveness

--- src/components/Courses/Course.js
+++ src/components/Courses/Course.js
@@ -5,13 +5,13 @@ const Course = ({ course }) => {
     console.log(course);
     return (
         <div className=''>
-            <div class="card w-80 bg-base-100 shadow-xl">
-                <figure class="px-10 pt-10">
-                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" class="rounded-xl" />
+            <div className="card w-80 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
                 </figure>
-                <div class="card-body">
-                    <h2 class="card-title">{course.name}</h2>
-                    <div className='flex items-center w-[20rem]'>
+                <div className="card-body">
+                    <h2 className="card-title mb-5">{course.name}</h2>
+                    <div className='flex items-center w-[20rem] mb-2'>
                         <p className='mr-3'>{course.gradguates}</p>
                         <div className='flex mr-16'>
                             <AiFillStar color='orange' />
@@ -21,8 +21,8 @@ const Course = ({ course }) => {
                             <AiFillStar color='orange' />
                         </div>
                     </div>
-                    <div class="card-actions">
-                        <button class="btn bg-[#FDEFE9] border-0 hover:bg-[#dbcbc4] text-[#F9835C]">Enroll Now</button>
+                    <div className="card-actions">
+                        <button className="btn bg-[#FDEFE9] border-0 hover:bg-[#dbcbc4] text-[#F9835C]">Enroll Now</button>
                     </div>
                 </div>
             </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,12 @@ import Teacher from '../../assets/images/teacher.svg';
 
 const Header = () => {
     return (
-        <header className='bg-gradient-to-r from-[#D4FEDC] to-[#EFF2DD] h-[89.6vh] mx-16 rounded-2xl flex items-center'>
-            <div className='ml-14'>
-                <h1 className='text-6xl font-semibold text-[#29312C] mb-5 w-4/6 leading-[4.5rem]'>Learn Great Things From Us</h1>
-                <p className='text-xl leading-9'>Online Certified Courses With in-depth and Practical Lessons in Over 100 Subjects. Sign Up For Free.</p>
-                <button className='px-4 py-3 rounded-lg bg-[#32D78B] text-white my-12'>Get Started Now</button>
-                <div className='flex justify-between w-[400px]'>
+        <header className='bg-gradient-to-r from-[#D4FEDC] to-[#EFF2DD] lg:h-[89.6vh] h-full lg:mx-16 rounded-2xl flex lg:flex-row flex-col items-center lg:py-0 py-5'>
+            <div className='lg:ml-14 mx-2'>
+                <h1 className='lg:text-6xl text-4xl font-semibold text-[#29312C] mb-5 lg:w-4/6 leading-[3.5rem] lg:leading-[4.5rem]'>Learn Great Things From Us</h1>
+                <p className='text-xl leading-9 lg:w-full w-[22rem]'>Online Certified Courses With in-depth and Practical Lessons in Over 100 Subjects. Sign Up For Free.</p>
+                <button className='px-4 py-3 rounded-lg bg-[#32D78B] text-white lg:my-12 my-8'>Get Started Now</button>
+                <div className='flex justify-between lg:flex-row flex-col w-[400px]'>
                     <div className='flex items-center'>
                         <BsArrowRight color='#32d788' className='mr-5'/><p className='inline font-medium'>Globally Recognised</p>
                     </div>
@@ -18,8 +18,8 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-            <div>
-                <img className='w-full' src={Teacher} alt="" />
+            <div className='lg:ml-0 ml-5 mt-10'>
+                <img className='lg:w-full w-3/4' src={Teacher} alt="" />
             </div>
         </header>
     );
